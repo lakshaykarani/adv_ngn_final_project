21ddfe8daf1be14824c3348f703af2c768ecec15
lakshay <lakshaykarani@gmail.com>
2018-04-26 19:06:25 -0600

    changes

--- Network_Visualization.py
+++ Network_Visualization.py
@@ -38,17 +38,89 @@ def bgp_view():
 
         quagga_bgp_routes = stdout_route.read()
         quagga_bgp_neighbors = stdout_neighbor.read()
-        
-        quagga_bgp_routes.replace("Removed","Removed<br><br>")
-        quagga_bgp_routes.replace("incomplete","incomplete<br><br>")
-        quagga_bgp_routes.replace("Path","Path<br><br>")
-        quagga_bgp_routes.replace("*>","<br><br>*>", 5)
-
-        quagga_bgp_neighbors.replace("memory","memory<br><br>")
-        quagga_bgp_neighbors.replace("State/PfxRcd","State/PfxRcd<br><br>")
-
         print(quagga_bgp_routes)
         print(quagga_bgp_neighbors)
+
+        quagga_bgp_routes = '''
+<table class="tg">
+  <tr>
+    <th class="tg-us36">Network</th>
+    <th class="tg-us36">Next Hop</th>
+    <th class="tg-us36">Metric</th>
+    <th class="tg-us36">LocPrf</th>
+    <th class="tg-us36">Weight</th>
+    <th class="tg-us36">Path</th>
+  </tr>
+  <tr>
+    <td class="tg-us36">*&gt; 10.0.0.0</td>
+    <td class="tg-us36">0.0.0.0</td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36"></td>
+    <td class="tg-us36">32768</td>
+    <td class="tg-us36">i</td>
+  </tr>
+  <tr>
+    <td class="tg-us36">*&gt; 11.0.0.0</td>
+    <td class="tg-us36">0.0.0.0</td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36"></td>
+    <td class="tg-us36">32768</td>
+    <td class="tg-us36">i</td>
+  </tr>
+  <tr>
+    <td class="tg-us36">*&gt; 20.0.0.0/24</td>
+    <td class="tg-us36">11.0.1.1</td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36"></td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36">300 i</td>
+  </tr>
+  <tr>
+    <td class="tg-us36">*&gt; 30.0.0.0/24</td>
+    <td class="tg-us36">11.0.1.1</td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36"></td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36">300 i</td>
+  </tr>
+  <tr>
+    <td class="tg-us36">*&gt; 40.0.0.0/24</td>
+    <td class="tg-us36">11.0.1.1</td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36"></td>
+    <td class="tg-us36">0</td>
+    <td class="tg-us36">300 i</td>
+  </tr>
+</table>'''
+
+
+        quagga_bgp_neighbors = '''<table class="tg">
+  <tr>
+    <th class="tg-us36">Neighbor</th>
+    <th class="tg-us36">V</th>
+    <th class="tg-us36">AS</th>
+    <th class="tg-us36">MsgRcvd</th>
+    <th class="tg-us36">MsgSent</th>
+    <th class="tg-us36">State/PfxRcd</th>
+  </tr>
+  <tr>
+    <td class="tg-us36">10.0.1.2</td>
+    <td class="tg-us36">4</td>
+    <td class="tg-us36">200</td>
+    <td class="tg-us36">4554</td>
+    <td class="tg-us36">4626</td>
+    <td class="tg-us36">0</td>
+  </tr>
+  <tr>
+    <td class="tg-us36">11.0.1.1</td>
+    <td class="tg-us36">4</td>
+    <td class="tg-us36">300</td>
+    <td class="tg-us36">14392</td>
+    <td class="tg-us36">14405</td>
+    <td class="tg-us36">3</td>
+  </tr>
+</table>'''
+
     finally:
         client.close()
 
