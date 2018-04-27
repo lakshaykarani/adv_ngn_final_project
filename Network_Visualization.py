4796656dd45d3bc98da82615d93089287b8b181c
lakshay <lakshaykarani@gmail.com>
2018-04-26 19:21:44 -0600

    changes

--- Network_Visualization.py
+++ Network_Visualization.py
@@ -4,7 +4,7 @@ import httplib
 import paramiko
 from json2html import *
 from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker
-
+from conf import *
 app = Flask(__name__, static_url_path='')
 
 
@@ -36,96 +36,17 @@ def bgp_view():
         stdin, stdout_route, stderr = client.exec_command("sudo vtysh -c 'show ip bgp'")
         stdin, stdout_neighbor, stderr = client.exec_command('sudo vtysh -c "show ip bgp summary"')
 
-        quagga_bgp_routes = stdout_route.read()
-        quagga_bgp_neighbors = stdout_neighbor.read()
-        print(quagga_bgp_routes)
-        print(quagga_bgp_neighbors)
+        bgp_routes = stdout_route.read()
+        bgp_neighbors = stdout_neighbor.read()
+        print(bgp_routes)
+        print(bgp_neighbors)
 
-        quagga_bgp_routes = '''
-<table class="tg">
-  <tr>
-    <th class="tg-us36">Network</th>
-    <th class="tg-us36">Next Hop</th>
-    <th class="tg-us36">Metric</th>
-    <th class="tg-us36">LocPrf</th>
-    <th class="tg-us36">Weight</th>
-    <th class="tg-us36">Path</th>
-  </tr>
-  <tr>
-    <td class="tg-us36">*&gt; 10.0.0.0</td>
-    <td class="tg-us36">0.0.0.0</td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36"></td>
-    <td class="tg-us36">32768</td>
-    <td class="tg-us36">i</td>
-  </tr>
-  <tr>
-    <td class="tg-us36">*&gt; 11.0.0.0</td>
-    <td class="tg-us36">0.0.0.0</td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36"></td>
-    <td class="tg-us36">32768</td>
-    <td class="tg-us36">i</td>
-  </tr>
-  <tr>
-    <td class="tg-us36">*&gt; 20.0.0.0/24</td>
-    <td class="tg-us36">11.0.1.1</td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36"></td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36">300 i</td>
-  </tr>
-  <tr>
-    <td class="tg-us36">*&gt; 30.0.0.0/24</td>
-    <td class="tg-us36">11.0.1.1</td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36"></td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36">300 i</td>
-  </tr>
-  <tr>
-    <td class="tg-us36">*&gt; 40.0.0.0/24</td>
-    <td class="tg-us36">11.0.1.1</td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36"></td>
-    <td class="tg-us36">0</td>
-    <td class="tg-us36">300 i</td>
-  </tr>
-</table>'''
-
-
-        quagga_bgp_neighbors = '''<table class="tg">
-  <tr>
-    <th class="tg-us36">Neighbor</th>
-    <th class="tg-us36">V</th>
-    <th class="tg-us36">AS</th>
-    <th class="tg-us36">MsgRcvd</th>
-    <th class="tg-us36">MsgSent</th>
-    <th class="tg-us36">State/PfxRcd</th>
-  </tr>
-  <tr>
-    <td class="tg-us36">10.0.1.2</td>
-    <td class="tg-us36">4</td>
-    <td class="tg-us36">200</td>
-    <td class="tg-us36">4554</td>
-    <td class="tg-us36">4626</td>
-    <td class="tg-us36">0</td>
-  </tr>
-  <tr>
-    <td class="tg-us36">11.0.1.1</td>
-    <td class="tg-us36">4</td>
-    <td class="tg-us36">300</td>
-    <td class="tg-us36">14392</td>
-    <td class="tg-us36">14405</td>
-    <td class="tg-us36">3</td>
-  </tr>
-</table>'''
 
     finally:
         client.close()
 
 	
-    return render_template('bgp_view.html',quagga_bgp_routes=Markup(quagga_bgp_routes), quagga_bgp_neighbors = Markup(quagga_bgp_neighbors))
+    return render_template('bgp_view.html',quagga_bgp_routes=Markup(quagga_bgp_routes), quagga_bgp_neighbors = Markup(quagga_bgp_neighbors),ryu_bgp_routes=Markup(quagga_bgp_routes), ryu_bgp_neighbors = Markup(quagga_bgp_neighbors))
 
 @app.route('/optimize', methods = ['GET', 'POST'])
 def optimize():
